c7a30161fa0dce69cc6cb7837e695cb9de4f1d75
Phạm Thị Mai <pham82755@gmail.com>
2023-11-20 17:32:38 +0700

    no message

--- assets/js/list-product-2.js
+++
@@ -1,9 +0,0 @@
-// Show more content
-const showMore = document.querySelector(".products-guide-btn");
-showMore.addEventListener("click", () => {
-  document.querySelector(".products-guide-content").classList.toggle("show");
-  const contentBtn = showMore.querySelector("span");
-  if (contentBtn.innerHTML == "Show More") {
-    contentBtn.innerHTML = "Hide";
-  } else contentBtn.innerHTML = "Show More";
-});
